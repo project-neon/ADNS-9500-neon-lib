0530d6c8ec62847222cfd90884702dfe9cd0cb87
Ana Luiza Fioravante Di Sciascio <analuiza197@gmail.com>
2024-02-12 20:48:20 -0300

    initial commit

---
+++ examples/MouseSensorADNS9500Example/MouseSensorADNS9500Example.ino
@@ -0,0 +1,20 @@
+#include "MouseSensorADNS9500.cpp"
+
+void setup() {
+  // Initialize Serial
+  Serial.begin(115200);
+
+  // Initialize Mouse Sensor
+  mouseInit();
+}
+
+void loop() {
+  // Read mouse motion
+  int16_t dx = 0;
+  int16_t dy = 0;
+  mouseReadXY(dx, dy);
+  Serial.print(dx);
+  Serial.print("   ");
+  Serial.println(dy);
+  delay(100);
+}
