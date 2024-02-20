d3562aa0f6d37e0c10449b019ae493a4c6968596
Eduardo Makita <makitayukio1@gmail.com>
2024-02-20 17:17:37 -0300

    feat(example): fix function call method, add cpi configuration and measure convertion functions

--- examples/MouseSensorADNS9500Example/MouseSensorADNS9500Example.ino
+++ examples/MouseSensorADNS9500Example/MouseSensorADNS9500Example.ino
@@ -1,18 +1,33 @@
-#include <MouseSensorADNS9500.hpp>
+#include "MouseSensorADNS9500.hpp"
+
+// value ranges from 90 up to 5040cpi
+int cpi = 1800;
+
+// converts measures from the sensor to cm, still not validated
+float_t CPItoCM(int16_t counts) {
+  return 2.54*(counts/((float_t) cpi));
+}
+
+// converts measures from the sensor to mm, still not validated
+float_t CPItoMM(int16_t counts) {
+  return 25.4*(counts/((float_t) cpi));
+}
 
 void setup() {
   // Initialize Serial
   Serial.begin(115200);
 
-  // Initialize Mouse Sensor
-  mouseInit();
+  // Initialize Mouse Sensor, desired cpi is required as argument
+  mouseInit(cpi);
 }
 
 void loop() {
   // Read mouse motion
   int16_t dx = 0;
   int16_t dy = 0;
-  mouseReadXY(dx, dy);
+
+  mouseReadXY(&dx, &dy);
+
   Serial.print(dx);
   Serial.print("   ");
   Serial.println(dy);
